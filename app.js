116c35af75c67ceb98b0f30be8ab435f3df05fb9
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2021-03-24 10:21:45 -0600

    non dry

---
+++ app.js
@@ -0,0 +1,10 @@
+function cow() {
+  document.getElementById('msg').innerText = 'The Cow Says Moo!'
+  document.getElementById('img').innerHTML = '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Cow_female_black_white.jpg/1200px-Cow_female_black_white.jpg" alt="cow" />'
+}
+
+
+function duck() {
+  document.getElementById('msg').innerText = 'The Duck Says Quack!'
+  document.getElementById('img').innerHTML = '<img src="https://www.treehugger.com/thmb/7g7LQAnUZcEWSThwdvIlFt2u2G0=/4560x2565/smart/filters:no_upscale()/duckling-close-up-500315849-572917c93df78ced1f0b99ec.jpg" alt="cow" />'
+}
