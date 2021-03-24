0ed6f238376edbc266dc7e6a0395c5eab1d8af57
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2021-03-24 10:26:02 -0600

    non-dry

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ function cow() {
 
 function duck() {
   document.getElementById('msg').innerText = 'The Duck Says Quack!'
-  document.getElementById('img').innerHTML = '<img src="https://www.treehugger.com/thmb/7g7LQAnUZcEWSThwdvIlFt2u2G0=/4560x2565/smart/filters:no_upscale()/duckling-close-up-500315849-572917c93df78ced1f0b99ec.jpg" alt="cow" />'
+  document.getElementById('img').innerHTML = '<img src="https://www.treehugger.com/thmb/7g7LQAnUZcEWSThwdvIlFt2u2G0=/4560x2565/smart/filters:no_upscale()/duckling-close-up-500315849-572917c93df78ced1f0b99ec.jpg" alt="duck" />'
 }
